8bea47d4b687b686edabcead71988a8883fdf0be
ironmaniiith <aalekhj2507@gmail.com>
2016-03-23 08:16:39 +0000

    importing several modules in GLOBALS.py itself

--- mines/GLOBALS.py
+++ mines/GLOBALS.py
@@ -2,7 +2,8 @@
 
 import cv2, numpy as np
 import sys, os
-import re, time, random, operator, collections
+import re, time, random, collections
+from operator import add, sub, mul, div, le, ge, lt, gt, abs
 import giveDimensions
 
 """
